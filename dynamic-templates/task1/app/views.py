58e9d669d3d94821fb3b97e890f5cca6d4c98b35
Shirenkov Roman <shirenkov_roman@mail.ru>
2021-04-18 04:16:57 +0300

    решение третьего задания/solution of the third task

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,24 @@
+import csv
 from django.shortcuts import render
+from django.conf import settings
+
 
 def inflation_view(request):
     template_name = 'inflation.html'
 
-    # чтение csv-файла и заполнение контекста
-    context = {}
+    tab = []
+    with open(settings.INFLATION_RUSSIA_CSV, newline='', encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        for i in reader:
+            row_list = []
+            for g in i:
+                if "." in g:
+                    row_list.append(float(g))
+                else:
+                    row_list.append((g))
+            tab.append(row_list)
+
+    context = {"tab": tab}
 
     return render(request, template_name,
                   context)
