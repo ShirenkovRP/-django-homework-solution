89d5c248d6a93b05e4fa90f02e15ae4d708f5203
Shirenkov Roman <shirenkov_roman@mail.ru>
2021-04-25 23:52:06 +0300

    решение четвертого задания/solving the fourth task

--- databases/work_with_database/phones/models.py
+++ databases/work_with_database/phones/models.py
@@ -3,4 +3,14 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+
+    name = models.TextField(verbose_name="Наименование")
+    price = models.DecimalField(verbose_name="Цена", max_digits=6, decimal_places=0)
+    image = models.URLField(verbose_name="Изображение")
+    release_date = models.DateField(verbose_name="Дата релиза")
+    lte_exists = models.BooleanField(verbose_name="Наличие")
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+    # pass
--- databases/work_with_database/phones/views.py
+++ databases/work_with_database/phones/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
+from phones.models import Phone
 
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get('sort')
+
+    if sort == 'name':
+        elements_db = Phone.objects.all().order_by('name')
+    elif sort == 'min_price':
+        elements_db = Phone.objects.all().order_by('price')
+    elif sort == 'max_price':
+        elements_db = Phone.objects.all().order_by('-price')
+    else:
+        elements_db = Phone.objects.all()
+
+    context = {"phones": elements_db}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    element_db = Phone.objects.filter(slug=slug)
+    context = {'telephone': element_db}
     return render(request, template, context)
