c41bfdd7ecd09e485b8868a1b3acb52ba0d57981
Shirenkov Roman <shirenkov_roman@mail.ru>
2021-05-04 23:13:50 +0300

    completed task 5.1/выполнил задание 5.1

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -1,13 +1,15 @@
 from django.views.generic import ListView
 from django.shortcuts import render
+from pprint import pprint
 
-from articles.models import Article
+from .models import Article, Badge, Relation
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
-
+    articles = Article.objects.all().order_by('-published_at')
+    related = Relation.objects.all()
+    context = {'object_list': articles, 'related': related}
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
--- databases_2/m2m-relations/website/settings.py
+++ databases_2/m2m-relations/website/settings.py
@@ -70,11 +70,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'website.wsgi.application'
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_m2m_relations',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -102,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Europe/Moscow'
 
@@ -130,4 +128,3 @@ try:
     from .settings_local import *
 except ImportError:
     pass
-
